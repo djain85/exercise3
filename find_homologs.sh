289c722eb4105498c636ccf3849c9d428eacb6d0
Diya Jain <djain85@gatech.edu>
2025-09-04 14:04:50 -0400

    fix output

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,4 +2,4 @@
 makeblastdb -in "$2" -dbtype nucl -out blastdb
 tblastn -query "$1" -db blastdb -out temp -outfmt "6 pident qlen length"
 awk '$1 > 30 && $3 / $2 > 0.9' temp > "$3"
-wc -l < "$3"
+grep -c . "$3"
