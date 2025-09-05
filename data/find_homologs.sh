76220151346e851e7ee816f3ddae64aa86182dbb
Diya Jain <djain85@gatech.edu>
2025-09-05 13:09:54 -0400

    Update script to output without a filename

---
+++ data/find_homologs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+outputfile="$3"
+tblastn -query "$1" -subject "$2" -out temp.txt -task tblastn-fast -outfmt "6 qseqid sseqid pident length qlen"
+awk '$3 > 30 && $4 / $5 > 0.9' temp.txt > "$outputfile"
+grep -c . "$outputfile"
